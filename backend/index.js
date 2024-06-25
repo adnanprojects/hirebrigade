2750cb265553d7226d1ffc39335c5f7d867a68b6
adnanprojects <adnanchaudhary99@gmail.com>
2024-06-25 21:16:26 +0530

    Add index file

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+const connectDB = require('./config/db');
+const auth = require('./routes/auth');
+const jobs = require('./routes/jobs');
+const applications = require('./routes/applications');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use('/api/auth', auth);
+app.use('/api/jobs', jobs);
+app.use('/api/applications', applications);
+
+// connection to DB
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server is running on port : ${PORT}`));
